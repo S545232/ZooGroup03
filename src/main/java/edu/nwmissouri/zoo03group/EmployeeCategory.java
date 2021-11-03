ebf6f26f9ba62a345b01c133047c51d56bc8f4dd
vinuthnachowdary <vikyasha7967@gmail.com>
2021-11-02 20:57:45 -0500

    Required Enum classes created for the project

---
+++ src/main/java/edu/nwmissouri/zoo03group/EmployeeCategory.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package edu.nwmissouri.zoo03group;
+
+/**
+ *
+ * @author Satya Sai Vinuthna Tummala
+ */
+public enum EmployeeCategory {
+    EMPLOYEE_CARE_TAKER,
+    EMPLOYEE_STAFF,
+    EMPLOYEE_RESCUE_OPEARTORS,
+    EMPLOYEE_GUIDE,
+    EMPLOYEE_VETERANS
+}
