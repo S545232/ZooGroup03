df616d0d2f3babc05db44420efd1bfae7df3c032
ANIL KOLLA <89354787+S545232@users.noreply.github.com>
2021-11-05 18:54:35 -0500

    function created for fisrt time visitor in main function

--- src/main/java/edu/nwmissouri/zoo03group/App.java
+++ src/main/java/edu/nwmissouri/zoo03group/App.java
@@ -3,14 +3,7 @@ package edu.nwmissouri.zoo03group;
 public class App {
 
     public static void main(String[] args) {
-
-        FirstTimeVisitor firstTimeVisitor = new FirstTimeVisitor("FirstTimeVisitor", "10$", "name", "email.com", 1234567890);
-        firstTimeVisitor.getName();
-        firstTimeVisitor.getEmail();
-        firstTimeVisitor.getPhoneNumber();
-        firstTimeVisitor.ticketPrice();
-        firstTimeVisitor.visitorCategory();
-
+        initFirstTimeVisitor();
         initStudentVisitor();
         initChildVisitor();
         initSeniorCitizenVisitor();
@@ -19,6 +12,15 @@ public class App {
         initEmployeeRescueOperator();
         initEmployeeGuide();
         initZooMaintainer();
+    }
+    
+    private static void initFirstTimeVisitor() {
+        FirstTimeVisitor firstTimeVisitor = new FirstTimeVisitor("FirstTimeVisitor", "10$", "name", "email.com", 1234567890);
+        firstTimeVisitor.getName();
+        firstTimeVisitor.getEmail();
+        firstTimeVisitor.getPhoneNumber();
+        firstTimeVisitor.ticketPrice();
+        firstTimeVisitor.visitorCategory();
     }
 
     private static void initStudentVisitor() {
