d60c2607d743e64cbbab69b9b055acb5781a2489
Baskar0106 <bhaskarbobbala2000@gmail.com>
2021-11-05 22:16:36 -0500

    generated javadoc

--- src/main/java/edu/nwmissouri/zoo03group/ChildVisitor.java
+++ src/main/java/edu/nwmissouri/zoo03group/ChildVisitor.java
@@ -5,29 +5,51 @@
 package edu.nwmissouri.zoo03group;
 
 /**
- *
+ * This is about Child visitor class
  * @author Bhaskar Venkata Prudhvi Bobbala
  */
 public class ChildVisitor extends Visitor{
+
+    /**
+     * constructor in the class
+     * @param visitorCategory
+     * @param ticketPrice
+     * @param name
+     * @param email
+     * @param phoneNumber
+     * @param age
+     * @param zooStroller
+     */
     public ChildVisitor(String visitorCategory, String ticketPrice, String name, String email, int phoneNumber, int age, String zooStroller) {
         super(visitorCategory, ticketPrice, name, email, phoneNumber);
     }
 
+    /**
+     *Override method to print Ticket Price
+     */
     @Override
     public void ticketPrice() {
         System.out.println("Zoo Entry Ticket price for child visitor is : 4$");
     }
 
+    /**
+     *Override method to print Visitor Category
+     */
     @Override
     public void visitorCategory() {
         System.out.println("I am a Child visitor");
     }
     
+    /**
+     *This method is used to print age of a Child Visitor
+     */
     public void age(){
         System.out.println("I am 4 years old");
     }
         
-    
+    /**
+     *This method is used to print Zoo Stroller for a Child Visitor
+     */
     public void zooStroller(){
         System.out.println("I need a Stroller");
     }  
--- src/main/java/edu/nwmissouri/zoo03group/SeniorCitizenVisitor.java
+++ src/main/java/edu/nwmissouri/zoo03group/SeniorCitizenVisitor.java
@@ -5,28 +5,50 @@
 package edu.nwmissouri.zoo03group;
 
 /**
- *
+ * This is about Senior citizen class
  * @author Bhaskar Venkata Prudhvi
  */
 public class SeniorCitizenVisitor extends Visitor {
+
+    /**
+     * Constructor for senior citizen class
+     * @param SeniorCitizenVisitor
+     * @param ticketPrice
+     * @param name
+     * @param email
+     * @param phoneNumber
+     * @param zooWheelChair
+     */
     public SeniorCitizenVisitor(String SeniorCitizenVisitor, String ticketPrice, String name, String email, int phoneNumber, String zooWheelChair) {
         super(SeniorCitizenVisitor, ticketPrice, name, email, phoneNumber);
     }
 
+    /**
+     *Override method to print Ticket Price
+     */
     @Override
     public void ticketPrice() {
         System.out.println("Zoo Entry Ticket price for the senior citizen visitor is : 5$");
     }
 
+    /**
+     *Override method to print Visitor Category
+     */
     @Override
     public void visitorCategory() {
         System.out.println("I am a Senior Citizen visitor");
     }
     
+    /**
+     *This method is used to print age of a Senior Citizen Visitor
+     */
     public void age(){
         System.out.println("I am 62 years old");
     }
     
+    /**
+     *This method is used to print Zoo Wheel Chair for a Senior Citizen Visitor
+     */
     public void zooWheelChair(){
         System.out.println("I need Wheel Chair");
     }  
