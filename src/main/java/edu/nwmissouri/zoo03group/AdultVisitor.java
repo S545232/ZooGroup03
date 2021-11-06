e271b5b0947f321299bd28c94e86b92554409036
Harimummadi <mummadiharihara9@gmail.com>
2021-11-05 22:14:18 -0500

    generated javadoc

--- src/main/java/edu/nwmissouri/zoo03group/AdultVisitor.java
+++ src/main/java/edu/nwmissouri/zoo03group/AdultVisitor.java
@@ -5,23 +5,42 @@
 package edu.nwmissouri.zoo03group;
 
 /**
- *
+ * This class is about Adult Visitor which visitor
  * @author Hari Hara Mummadi
  */
 public class AdultVisitor extends Visitor{
 
+    /**
+     * Constructor for Adult Visitor Class
+     * @param visitorCategory
+     * @param ticketPrice
+     * @param name
+     * @param email
+     * @param phoneNumber
+     */
     public AdultVisitor(String visitorCategory, String ticketPrice, String name, String email, int phoneNumber) {
         super(visitorCategory, ticketPrice, name, email, phoneNumber);
     }
+
+    /**
+     * Over riding the Ticket Price For Adult visitor
+     */
     @Override
     public void ticketPrice() {
         System.out.println("Zoo Entry Ticket price for the Adult Visitor is : 8.5$");
     }
 
+    /**
+     * Over riding the Visitor Category for Adult Visitor 
+     */
     @Override
     public void visitorCategory() {
         System.out.println("I am an Adult Visitor");
     }
+
+    /**
+     * Method for Visitor Occupation of Adult Visitor
+     */
     public void visitorOccupation(){
         System.out.println("Adult Visitor Occupation is : Sales Manager");
     }
--- src/main/java/edu/nwmissouri/zoo03group/EmployeeCareTaker.java
+++ src/main/java/edu/nwmissouri/zoo03group/EmployeeCareTaker.java
@@ -5,29 +5,48 @@
 package edu.nwmissouri.zoo03group;
 
 /**
- *
+ * This class is about Employee Care Taker
  * @author Hari Hara Mummadi
  */
 public class EmployeeCareTaker extends Employee {
 
+    /**
+     * Constructor for employee care taker class
+     * @param employeeID
+     * @param employeeCategory
+     * @param employeeName
+     * @param employeeShiftTimings
+     * @param name
+     * @param email
+     * @param phoneNumber
+     */
     public EmployeeCareTaker(int employeeID, String employeeCategory, String employeeName, String employeeShiftTimings, String name, String email, int phoneNumber) {
         super(employeeID, employeeCategory, employeeName, employeeShiftTimings, name, email, phoneNumber);
     }
+ /**
+  * Over riding the method to print the employee shift timings
+  */
 
     @Override
     public void employeeShiftTimings() {
         System.out.println("Employee shift timings are : 9AM - 5PM");
     }
-
+/**
+ * Over riding the method to print the employee name
+ */
     @Override
     public void employeeName() {
         System.out.println("Employee Name : David");
     }
-
+/**
+ * Over riding the method to print the employee category
+ */
     @Override
     public void employeeCategory() {
         System.out.println("I am an Care Taker in the Zoo");    }
-
+/**
+ * Over riding the method to print the employee ID
+ */
     @Override
     public void employeeID() {
         System.out.println("My Employee ID is : 865432");    }
