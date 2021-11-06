0a02f543e7ca54758b9aaeb945d8f779885c98e4
ANIL KOLLA <89354787+S545232@users.noreply.github.com>
2021-11-05 22:08:13 -0500

    jovadoc generated

--- src/main/java/edu/nwmissouri/zoo03group/AbstractPerson.java
+++ src/main/java/edu/nwmissouri/zoo03group/AbstractPerson.java
@@ -5,7 +5,7 @@
 package edu.nwmissouri.zoo03group;
 
 /**
- *
+ * An abstract class which contains abstract and implemented functions 
  * @author Anil Kumar Kolla(S545232@nwmissouri.edu)
  */
 public abstract class AbstractPerson {
@@ -14,18 +14,37 @@ public abstract class AbstractPerson {
     private String email;
     private int phoneNumber;
 
+    /**
+     * Constructor with three parameters accepting name, email and phone number of a person
+     * @param name
+     * @param email
+     * @param phoneNumber
+     */
     public AbstractPerson(String name, String email, int phoneNumber) {
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * An abstract function to get the name
+     */
     public abstract void getName();
 
+    /**
+     * An abstract function to get the email
+     */
     public abstract void getEmail();
 
+    /**
+     * An abstract function to get the phone number
+     */
     public abstract void getPhoneNumber();
 
+    /**
+     * This  function is to print the constructor parameters
+     * @return
+     */
     @Override
     public String toString() {
         return "Person{" + "Name is : " + name + ", Email is : " + email + " and PhoneNumber is : " + phoneNumber + '}';
--- src/main/java/edu/nwmissouri/zoo03group/FirstTimeVisitor.java
+++ src/main/java/edu/nwmissouri/zoo03group/FirstTimeVisitor.java
@@ -5,20 +5,34 @@
 package edu.nwmissouri.zoo03group;
 
 /**
- *
+ * This class which contains Override functions to print the First time visitor 
  * @author Anil Kumar Kolla(S545232@nwmissouri.edu)
  */
 public class FirstTimeVisitor extends Visitor{
 
+    /**
+     * Constructor with parameters to represent the behavior of a first time visitor
+     * @param visitorCategory
+     * @param ticketPrice
+     * @param name
+     * @param email
+     * @param phoneNumber
+     */
     public FirstTimeVisitor(String visitorCategory, String ticketPrice, String name, String email, int phoneNumber) {
         super(visitorCategory, ticketPrice, name, email, phoneNumber);
     }
 
+    /**
+     * An Override function which prints the ticket price for a first time visitor
+     */
     @Override
     public void ticketPrice() {
         System.out.println("Zoo Entry Ticket price for the first time visitor is : 10$");
     }
 
+    /**
+     * An Override function which prints the category of a first time visitor
+     */
     @Override
     public void visitorCategory() {
         System.out.println("I am a First Time Visitor");
